a71f5d50e8c9440742a5675ff67c8c974cc0fb93
Pawel Salamonczyk <salamonczykpawel@gmail.com>
2020-02-29 10:58:19 +0100

    test

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,5 +7,15 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         BarChart barChart = new BarChart("Task types report",new ArrayList());
+
+        BarChart barChart1 = BarChart.builder()
+                .withTitle("Test")
+                .withFontName("Arial")
+                .withFontSize(15)
+                .withHeight(12)
+                .withWidth(10)
+                .build();
+
+        System.out.println(barChart1.toString());
     }
 }
